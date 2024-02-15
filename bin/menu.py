b34307c31a29368800c8febd85580b2dd434e13f
EneFixx <152084457+EneFixx@users.noreply.github.com>
2024-02-15 14:49:07 +0100

    Update menu.py

--- bin/menu.py
+++ bin/menu.py
@@ -1,4 +1,14 @@
 import pygame as pg
 
-def menu_display():
-    pass
+color = (80,80,80,128)
+couleur = pg.Color(0,0,0,a=120)
+
+
+def menu_display(screen,screen_width,screen_height):
+    
+    
+
+    screen.fill((0,0,0,128))
+    pg.draw.rect(screen, color, pg.Rect((screen_width/2 - 250), (screen_height/7), 500, 160))
+    pg.draw.rect(screen, color, pg.Rect((screen_width/2 - 250), (screen_height/7 + 2*screen_height/7 ), 500, 160))
+    pg.draw.rect(screen, color, pg.Rect((screen_width/2 - 250), (screen_height/7 + 4*screen_height/7 ), 500, 160))
